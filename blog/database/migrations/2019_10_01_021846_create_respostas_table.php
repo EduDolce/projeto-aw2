69870062f4010bae7e37f803fd0f156e0e563f50
Kayane Rocha <kayannerch@gmail.com>
2019-09-30 23:27:06 -0300

    subindo as modificacoes

---
+++ blog/database/migrations/2019_10_01_021846_create_respostas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRespostasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('respostas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('texto');
+            $table->timestamps('data');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('respostas');
+    }
+}
