ad8b9ae9416b6609508e227a27efca63658c5706
Kayane Rocha <kayannerch@gmail.com>
2019-11-14 17:17:42 -0300

    model e migrations de paciente e solicitacao

--- blog/database/migrations/2019_11_14_195717_create_pacientes_table.php
+++ blog/database/migrations/2019_11_14_195717_create_pacientes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRespostasTable extends Migration
+class CreatePacientesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateRespostasTable extends Migration
      */
     public function up()
     {
-        Schema::create('respostas', function (Blueprint $table) {
+        Schema::create('pacientes', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
-            $table->string('texto');
-            $table->timestamps('data');
         });
     }
 
@@ -28,6 +26,6 @@ class CreateRespostasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('respostas');
+        Schema::dropIfExists('pacientes');
     }
 }
