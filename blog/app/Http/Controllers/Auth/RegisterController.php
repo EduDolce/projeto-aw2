bb8fda4e31870753388c6004a58106567ec76ecc
Kayane Rocha <kayannerch@gmail.com>
2019-11-20 20:10:35 -0300

    arrumando cadastro de usuario

--- blog/app/Http/Controllers/Auth/RegisterController.php
+++ blog/app/Http/Controllers/Auth/RegisterController.php
@@ -63,12 +63,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        User::create([
+        return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
 
-        return 'Cadastro completo.';
+        
     }
 }
